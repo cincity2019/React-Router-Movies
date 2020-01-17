40397317f3f2d6532f8ccca3fad85dd683529c77
cincity2019 <cnkelly73@gmail.com>
2020-01-16 21:52:27 -0500

    not done

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import MovieCard from './Movies/MovieCard';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [ savedList, setSavedList ] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = (movie) => {
+		setSavedList([ ...savedList, movie ]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+
+			<Route exact path="/Home" component={MovieList} />
+			<Route exact path="movie" component={Movie} />
+			<Route path="/movies/:id" component={Movie} />
+		</div>
+	);
 };
-
 export default App;
